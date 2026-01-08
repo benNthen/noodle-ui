17c22659ff8e112c172560de644fd78bfbf505cb
Watkins <tmh.pug@gmail.com>
2026-01-08 09:46:19 +0700

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    'localhost',
+    '127.0.0.1',
+    '192.168.0.0/16',
+    '10.0.0.0/8',
+  ],
   images: {
     remotePatterns: [
       {
